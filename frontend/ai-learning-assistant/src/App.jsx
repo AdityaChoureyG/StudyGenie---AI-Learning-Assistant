468b399400e88af65d175582e2da1cfb325a4db7
AdityaChoureyG <adityachourey8319@gmail.com>
2026-02-19 19:11:52 +0530

    frontend file structure

---
+++ frontend/ai-learning-assistant/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className='text-5xl text-emerald-600'>App</div>
+  )
+}
+
+export default App
